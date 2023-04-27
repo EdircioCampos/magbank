0422749c7e4a384c66ed85edcc35b4153e2ea823
edircio <edircio@yahoo.com.br>
2023-04-26 21:36:26 -0300

    navbar finalizada

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navigation'
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+    </div>
+    
+  );
+}
+
+export default App;
